c72ccca8f6ff907524bf2afce89d54dedabd7dd9
TheGucciOtter <44411224+TheGucciOtter@users.noreply.github.com>
2026-05-28 11:55:52 +0200

    Added the sql database and the wikipage scraper

---
+++ WIKIDLE_REPO/wiki_article_database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Wikipedia_articles (
+	article_id int PRIMARY KEY,
+	title VARCHAR(255),
+	summary TEXT,
+	category VARCHAR(100),
+	picture TEXT)
+
+CREATE TABLE Player (
+	player_id int PRIMARY KEY
+)
+
+CREATE TABLE Daily_game (
+	date DATE PRIMARY KEY
+)
+
+CREATE TABLE Plays (
+	guess_amount INT,
+	player_id INT REFERENCES Player(player_id) ON DELETE CASCADE,
+	date DATE REFERENCES Daily_game(date) ON DELETE CASCADE
+	)
---
+++ WIKIDLE_REPO/wiki_download_test.py
@@ -0,0 +1,20 @@
+import wikipediaapi
+import csv
+
+wiki_api = wikipediaapi.Wikipedia(user_agent='WikiProject (bcgamingdk@gmail.com)', language='en')
+
+
+article_titles = ['Python_(programming_language)', 'Michael Jordan']
+
+headers = ['title', 'summary', 'category']
+article_elements = []
+
+for article in article_titles:
+    wiki_page = wiki_api.page(article)
+    article_elements.append([wiki_page.title, wiki_page.summary, wiki_page.categories])
+
+with open('wiki_articles.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+    writer.writerows(article_elements)
+
