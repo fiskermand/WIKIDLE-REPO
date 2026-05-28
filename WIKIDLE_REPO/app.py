594814e5c748a4bdf820eaa866d42b5f749778d9
Lucas-coder28 <lucashelmoe@gmail.com>
2026-05-28 17:10:26 +0200

    Added place holder images to guesses

--- WIKIDLE_REPO/app.py
+++ WIKIDLE_REPO/app.py
@@ -37,7 +37,7 @@ example_dict = {
             "A member of the Republican Party, he served as the 45th president "
             "from 2017 to 2021..."
         ),
-        "wiki_picture": "test"
+        "wiki_picture": "images/President_Donald_J._Trump.jpg.webp"
     },
 
     "Mount Everest": {
@@ -50,7 +50,7 @@ example_dict = {
             "marks part of the China–Nepal border at its summit.[4] Its height was most recently measured in 2020 through a joint "
             "survey by Nepalese and Chinese authorities as 8,848.86 m (29,031 ft 8+1⁄2 in)..."
         ),
-        "wiki_picture": "test"
+        "wiki_picture": "images/Mt._Everest.jpg"
     },
 
     "Korean War": {
@@ -63,7 +63,7 @@ example_dict = {
             " (Republic of Korea; ROK) and their allies. North Korea was supported by China and the Soviet Union, "
             "while South Korea was supported by the United Nations led by the United States under the auspices of the United Nations Command (UNC)."
         ),
-        "wiki_picture": "test"
+        "wiki_picture": "images/Korean_War_Chosin.jpg"
     },
 }
 
@@ -76,6 +76,7 @@ def home():
     invalid_guess = False
     prev_guess = False
     image_blur = "blurred"
+    wiki_picture = ""
 
     guess_name = ""
     guess_category = ""
@@ -100,6 +101,7 @@ def home():
         wiki_category = example_dict[wiki_page]["wiki_category"]
         wiki_theme = example_dict[wiki_page]["wiki_theme"]
         wiki_name_blurred = re.sub(r"\S", "_", wiki_name)
+        wiki_picture = example_dict[wiki_page]["wiki_picture"]
 
     if request.method == "POST":
         action = request.form.get("action")
@@ -119,7 +121,7 @@ def home():
             wiki_text = example_dict[wiki_page]["wiki_text"]
             wiki_category = example_dict[wiki_page]["wiki_category"]
             wiki_theme = example_dict[wiki_page]["wiki_theme"]
-            wiki_picture = example_dict["Michael Jackson"]["wiki_picture"]
+            wiki_picture = example_dict[wiki_page]["wiki_picture"]
             wiki_name_blurred = re.sub(r"\S", "_", wiki_name)
 
         elif action == "reset":
@@ -242,6 +244,8 @@ def home():
         prev_guess=prev_guess,
         game_state=game_state,
         wiki_page=wiki_page,
+        wiki_picture=wiki_picture,
+        image_blur=image_blur,
         image_revealed=session.get("image_revealed", False)
     )
 
