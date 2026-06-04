3b123d1a83414060fb19d5e6799e76ac1a0910a2
Lucas-coder28 <lucashelmoe@gmail.com>
2026-06-04 11:42:42 +0200

    changed Connection.py and added EXAMPLE.env
    to run app.py, change "DB_PASSWORD" in "EXAMPLE.env" to your own PostgreSQL password and rename "EXAMPLE.env" to ".env"

--- WIKIDLE_REPO/connection.py
+++ WIKIDLE_REPO/connection.py
@@ -1,12 +1,13 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 connection = psycopg2.connect(
-    host="localhost",
-    user="postgres",
-    password="2807",
-    dbname="wikipedia_articles",
-    port=5432
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    dbname=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT")
 )
-
-cursor = connection.cursor()
-print("hell yeah")
